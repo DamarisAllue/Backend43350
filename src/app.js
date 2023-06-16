b7a3c2d6bd3e42396ff7165365c7e80ccc9d895c
Damaris Allue <116525982+DamarisAllue@users.noreply.github.com>
2023-06-16 01:07:03 -0300

    entrega 16/6

--- src/app.js
+++ src/app.js
@@ -1,19 +1,56 @@
 import express from 'express'
+import handlebars from 'express-handlebars'
+import { Server } from 'socket.io'
 import productRouter from './routers/products.routes.js'
 import cartRouter from './routers/carts.routes.js'
 import multer from 'multer'
+import __dirname, { PORT } from "./utils.js";
+import viewsProductsRoutes from "./routers/views.routes.js";
 
 const app = express()
 
+app.use(express.json());
+
+// Iniciar el servidor HTTP
+const serverHttp = app.listen(PORT, () =>
+    console.log(`Listening on port ${PORT}`)
+);
+// Crear una instancia de Socket.IO y vincularla al servidor HTTP
+const io = new Server(serverHttp);
+
+app.set("socketio", io);
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('./public'))
+
+// Configurar el motor de plantillas Handlebars
+app.engine("handlebars", handlebars.engine());
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
+
+// Ruta principal
+app.get("/", (req, res) => res.render("index", { name: "Dami" }))
+// Rutas para la API de productos y carritos
+app.use("/api/products", productRouter);
+app.use("/api/carts", cartRouter);
+// Ruta para las vistas de productos
+app.use("/home", viewsProductsRoutes);
+
+// Evento de conexión de Socket.IO
+io.on("connection", socket => {
+    console.log("Successful Connection");
+    // Escucha el evento "productList" emitido por el cliente
+    socket.on("productList", data => {
+        // Emitir el evento "updatedProducts" a todos los clientes conectados
+        io.emit("updatedProducts", data);
+    });
+});
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, './src/public')
     },
-    filename: (req, file,cb) => {
+    filename: (req, file, cb) => {
         cb(null, file.originalname)
     }
 })
---
+++ src/routers/views.routes.js
@@ -0,0 +1,32 @@
+import { Router } from 'express'
+
+const users = [
+    { id: 1, name: 'Alex', role: 'teacher' },
+    { id: 2, name: 'Damaris', role: 'student' },
+    { id: 3, name: 'Damian', role: 'student' },
+]
+
+const foods = [
+    { id: 1, name: 'ice cream' },
+    { id: 2, name: 'Grapes' },
+    { id: 3, name: 'Beer' },
+]
+
+const router = Router()
+
+router.get('/', (req, res) => {
+    // res.json({ users })
+    res.render('showUsers', {styles: 'blue.css', users})
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    const user = users.find(item => item.id == id)
+    res.render('showUser', {
+        user,
+        isTeacher: user.role == 'teacher' ? true : false,
+        foods
+    })
+})
+
+export default router
